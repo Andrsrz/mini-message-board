5d9d0cbf9a88ddfa86d3cd665475428cdc4fc19b
Andres Ruiz <andrsruiz@gmail.com>
2020-07-04 10:53:21 -0500

    Add local messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+	{
+		text: "Hi, there!",
+		user: "Andres",
+		added: new Date()
+	},
+	{
+		text: "Hello, Express!",
+		user: "Charles",
+		added: new Date()
+	}
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Messages',
+						messages: messages });
 });
 
 module.exports = router;
